ebdaae8e32106f0e7a6fad7bfe3df7188b247c6e
JordyAraujo <jordyaraujo@outlook.com>
2018-08-28 21:46:19 -0300

    Started developing the Modal functions

--- src/components/content/portfolio/Portfolio.js
+++ src/components/content/portfolio/Portfolio.js
@@ -3,12 +3,51 @@ import "./Portfolio.css";
 import PhotoGrid from './grid/PhotoGrid';
 
 class Portfolio extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+
+  handleClickBaja2013(){
+    
+  }
+
+  handleClickBaja2014(){
+    
+  }
+
+  handleClickBaja2016(){
+    
+  }
+
+  handleClickMIT(){
+    
+  }
+
+  handleClickWSU(){
+    
+  }
+
+  handleClickAresta(){
+    
+  }
+
   render() {
     return (
       <div className="w3-padding-32 w3-content" id="portfolio">
         <h2 className="w3-text-grey">Portfólio</h2>
         <hr className="w3-opacity" />
-        <PhotoGrid />
+        <PhotoGrid
+          onClickBaja2013={() => this.handleClickBaja2013()}
+          onClickBaja2014={() => this.handleClickBaja2014()}
+          onClickBaja2016={() => this.handleClickBaja2016()}
+          onClickMIT={() => this.handleClickMIT()}
+          onClickWSU={() => this.handleClickWSU()}
+          onClickAresta={() => this.handleClickAresta()}
+        />
       </div>
     );
   }
--- src/components/content/portfolio/grid/PhotoGrid.js
+++ src/components/content/portfolio/grid/PhotoGrid.js
@@ -7,83 +7,52 @@ class PhotoGrid extends Component {
             <div className="w3-row-padding" style={{ margin: "0 -16px" }}>
                 <div className="w3-half">
                     <div className="container">
-                        <button
-                            id="btn2013"
-                            text="XIX Competição Baja SAE Brasil"
-                            className="overlay text"
-                            style={{ cursor: "pointer" }}
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/baja2013.jpg"
                             alt="Baja 2013"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickBaja2013}
                         />
                     </div>
 
                     <div className="container">
-                        <button
-                            id="btnWSU"
-                            text="Wayne State University"
-                            className="overlay text"
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/wsu.jpg"
                             alt="Wayne State University"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickWSU}
                         />
                     </div>
 
                     <div className="container">
-                        <button
-                            id="btn2016"
-                            text="XXII Competição Baja SAE Brasil"
-                            className="overlay text"
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/baja2016.jpg"
                             alt="Baja 2016"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickBaja2016}
                         />
                     </div>
                 </div>
 
                 <div className="w3-half">
                     <div className="container">
-                        <button
-                            id="btn2014"
-                            text="XX Competição Baja SAE Brasil"
-                            className="overlay text"
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/baja2014.jpg"
                             alt="Baja 2014"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickBaja2014}
                         />
                     </div>
 
                     <div className="container">
-                        <button
-                            id="btnMIT"
-                            text="Massachusetts Institute of Technology"
-                            className="overlay text"
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/mit.jpg"
                             alt="Massachusetts Institute Of Technology"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickMIT}
                         />
                     </div>
 
                     <div className="container">
-                        <button
-                            id="btnAresta"
-                            text="Estágio na Aresta 3D"
-                            className="overlay text"
-                        />
                         <img
                             src="https://raw.githubusercontent.com/JordyAraujo/portfolio/master/src/img/aresta.jpg"
                             alt="Aresta 3D"
-                            style={{ width: "100%" }}
+                            onClick={this.props.onClickAresta}
                         />
                     </div>
                 </div>
